617cb8087e3d114ea236b9cdcd290529a23252a9
guiwatanabe <guiwatanabe@hotmail.com>
2026-03-18 15:39:30 -0300

    style: lint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,20 +6,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login'])->name('login')->middleware('throttle:5,1,api-login');
-
     Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
-
     Route::get('user', [AuthController::class, 'user'])->middleware('auth:sanctum')->name('user');
-
     Route::post('refresh-token', [AuthController::class, 'refresh'])->name('refresh-token');
-
     Route::post('register', [AuthController::class, 'register'])->name('register');
-
     Route::post('profile', [AuthController::class, 'updateProfile'])->middleware('auth:sanctum')->name('profile.update');
 });
 
 Route::prefix('teams')->middleware('auth:sanctum')->group(function () {
-
     Route::get('/', [TeamController::class, 'index'])->name('teams.index');
     Route::get('/{id}', [TeamController::class, 'show'])->whereNumber('id')->name('teams.show');
     Route::post('/', [TeamController::class, 'store'])->name('teams.store');
